8637f9e46497a22d2cbb4916e52948ac2c8a654a
Unknown <dougbuck@softwaretestingservices.co.uk>
2018-04-19 09:08:34 +0100

    1

--- test.py
+++ test.py
@@ -10,3 +10,41 @@
 #
 # print(currencyConverter(1.45,1000))
 #
+# dict = {"name":"Audit","profession":"Instructor"}
+# print (dict["name"])
+#
+# a = list(dict.values())
+# print (a)
+
+# v = 3
+# if v<3:
+#     print("less")
+# elif v ==3:
+#     print("equal")
+# else:
+#     print("greater")
+# a=2
+# print("positive" if a>0 else "negative")
+#
+# emails=['me@gmail.com','you@hotmail.com','they@gmail.com']
+# for email in emails:
+#     if 'gmail' in email:
+#         print(email)
+
+# planet=input("what planet are you from?")
+# print(planet)
+#
+# def currencyConverter(rate,euros):
+#     dollars = euros*rate
+#     return dollars
+# r=input("enter rate: ")
+# e=input("enter euros: ")
+# print(currencyConverter(float(r),float(e))
+#
+
+# names=['james','john','jake']
+# emailDomains=['gmail','hotmail','yahoo']
+#
+# for i,j in zip(names,emailDomains):
+#     print(i,j)
+
