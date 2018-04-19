a45541bac847b9324b2c3fbe6e372272e5f0ef0d
Unknown <dougbuck@softwaretestingservices.co.uk>
2018-04-19 08:17:34 +0100

    initial

---
+++ test.py
@@ -0,0 +1,12 @@
+#print("First Program")
+
+#Replaced = ('Audit'.replace('A','O'))
+
+#print(Replaced)
+#
+# def currencyConverter(rate,euros):
+#     dollars = euros*rate
+#     return dollars
+#
+# print(currencyConverter(1.45,1000))
+#
